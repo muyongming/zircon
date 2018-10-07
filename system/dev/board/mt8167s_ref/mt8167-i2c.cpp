207deef3ab58a982a6c267e4d9b5395abea02375
Mike Voydanoff <voydanoff@google.com>
2018-10-07 15:00:40 -0700

    add interrupts to board driver
    Change-Id: I4cd864a1e599420623ac1fa021d60511758a32d4

--- system/dev/board/mt8167s_ref/mt8167-i2c.cpp
+++ system/dev/board/mt8167s_ref/mt8167-i2c.cpp
@@ -13,29 +13,43 @@
 
 namespace board_mt8167 {
 
+const pbus_mmio_t i2c_mmios[] = {
+    {
+        .base = MT8167_I2C0_BASE,
+        .length = MT8167_I2C0_SIZE,
+    },
+    {
+        .base = MT8167_I2C1_BASE,
+        .length = MT8167_I2C1_SIZE,
+    },
+    {
+        .base = MT8167_I2C2_BASE,
+        .length = MT8167_I2C2_SIZE,
+    },
+    // MMIO for clocks
+    // TODO: move this to a clock driver
+    {
+        .base = MT8167_XO_BASE,
+        .length = MT8167_XO_SIZE,
+    },
+};
+
+const pbus_irq_t i2c_irqs[] = {
+    {
+    .irq = MT8167_I2C0_IRQ,
+    .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
+    },
+    {
+        .irq = MT8167_I2C1_IRQ,
+        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
+    },
+    {
+        .irq = MT8167_I2C2_IRQ,
+        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
+    },
+};
+
 zx_status_t Mt8167::I2cInit() {
-
-    const pbus_mmio_t i2c_mmios[] = {
-        {
-            .base = MT8167_I2C0_BASE,
-            .length = MT8167_I2C0_SIZE,
-        },
-        {
-            .base = MT8167_I2C1_BASE,
-            .length = MT8167_I2C1_SIZE,
-        },
-        {
-            .base = MT8167_I2C2_BASE,
-            .length = MT8167_I2C2_SIZE,
-        },
-        // MMIO for clocks
-        // TODO: move this to a clock driver
-        {
-            .base = MT8167_XO_BASE,
-            .length = MT8167_XO_SIZE,
-        },
-    };
-
     pbus_dev_t i2c_dev = {};
     i2c_dev.name = "i2c0";
     i2c_dev.vid = PDEV_VID_MEDIATEK;
@@ -43,6 +57,8 @@ zx_status_t Mt8167::I2cInit() {
     i2c_dev.did = PDEV_DID_MEDIATEK_I2C;
     i2c_dev.mmios = i2c_mmios;
     i2c_dev.mmio_count = countof(i2c_mmios);
+    i2c_dev.irqs = i2c_irqs;
+    i2c_dev.irq_count = countof(i2c_irqs);
 
     zx_status_t status = pbus_.ProtocolDeviceAdd(ZX_PROTOCOL_I2C_IMPL, &i2c_dev);
     if (status != ZX_OK) {
